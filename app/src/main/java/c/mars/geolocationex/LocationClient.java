5a5bbcbac87c60b758e17cd887a7e7e7248ced54
Constantine Mars <constantine.mars@gmail.com>
2015-06-11 18:53:04 +0300

    location client connection

---
+++ app/src/main/java/c/mars/geolocationex/LocationClient.java
@@ -0,0 +1,58 @@
+package c.mars.geolocationex;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import lombok.Data;
+
+/**
+ * Created by mars on 6/11/15.
+ */
+@Data
+public class LocationClient {
+    final private GoogleApiClient client;
+    final private Context context;
+    final private Callbacks callbacks;
+    private GoogleApiClient.ConnectionCallbacks connectionCallbacks=new GoogleApiClient.ConnectionCallbacks() {
+        @Override
+        public void onConnected(Bundle bundle) {
+            callbacks.connected(true);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+    };
+    private GoogleApiClient.OnConnectionFailedListener failedListener=new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            callbacks.connected(false);
+        }
+    };
+
+    public LocationClient(Context context, Callbacks callbacks) {
+        this.context = context;
+        this.callbacks = callbacks;
+        client=new GoogleApiClient.Builder(this.context)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this.connectionCallbacks)
+                .addOnConnectionFailedListener(this.failedListener)
+                .build();
+    }
+
+    public void connect(){
+        client.connect();
+    }
+
+    public void disconnect(){
+        client.disconnect();
+    }
+    interface Callbacks{
+        void connected(boolean b);
+    }
+}
--- app/src/main/java/c/mars/geolocationex/MainActivity.java
+++ app/src/main/java/c/mars/geolocationex/MainActivity.java
@@ -1,10 +1,7 @@
 package c.mars.geolocationex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -15,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.t)
     TextView t;
+    private boolean b;
+    private LocationClient locationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
         Timber.plant(new Timber.DebugTree());
-        t.setOnClickListener(v -> t.setText("clicked"));
+
+        t.setOnClickListener(v -> t.setText("clicked: "+Boolean.toString(b=!b)));
+        locationClient=new LocationClient(this, b1 -> {
+            String s="connected: "+ b1;
+            Timber.d(s);
+            t.setText(s);
+        });
+        locationClient.connect();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        locationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        locationClient.disconnect();
+        super.onStop();
     }
 }
